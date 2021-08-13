46de685f8dd77e0db3254b96ef259be2dc193f63
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-08-13 12:26:16 +0300

    Init

---
+++ Как я устал от JavaScript и создал свой собственный язык программирования _ Хабр_files/ads.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+document.addEventListener('DOMContentLoaded', function() {
+  var e=document.createElement('div');e.id='K9Jn4i4Yhz';e.style.display='none';document.body.appendChild(e);
+});
